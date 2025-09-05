43e3bcc27695bb07000db796f9cdffa750937f05
Jeshaiah73 <jedrac46@gmail.com>
2025-09-05 15:57:42 +0700

    MongoDB Connection String and Environment
    database connection and make sure the connection string URL -> .env -> string -> dotenv

--- nodejs-ecommerce-api/app/app.js
+++ nodejs-ecommerce-api/app/app.js
@@ -1,5 +1,10 @@
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
+import dbConnect from "../config/dbConnect.js"; 
 
+dotenv.config()
+//database connect
+dbConnect();
 const app = express();
 
 export default app;
---
+++ nodejs-ecommerce-api/config/dbConnect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+    try {
+        const connected = await mongoose.connect(process.env.MONGO_URL);
+        mongoose.set('strictQuery', true);
+        console.log(`MongoDB Connected: ${connected.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default dbConnect;
