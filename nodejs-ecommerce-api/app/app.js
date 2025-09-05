c66c68deb4947bc31fca36b678d1269709706833
Jeshaiah73 <jedrac46@gmail.com>
2025-09-05 16:52:55 +0700

    User Registration Controller + Hash User Password

--- nodejs-ecommerce-api/app/app.js
+++ nodejs-ecommerce-api/app/app.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
 import dbConnect from "../config/dbConnect.js"; 
+import userRoutes from "../routes/usersRoute.js";
 
 dotenv.config()
+
 //database connect
 dbConnect();
 const app = express();
+
+//pass incoming data
+app.use(express.json());
+
+//routes
+app.use('/', userRoutes);
 
 export default app;
