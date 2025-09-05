f1ea0fe561d6be65023c40922b70ca3be0181182
Jeshaiah73 <jedrac46@gmail.com>
2025-09-06 00:33:35 +0700

    All Updates for Routes, Middlewares, Controller and Model

--- nodejs-ecommerce-api/middlewares/globalErrHandler.js
+++ nodejs-ecommerce-api/middlewares/globalErrHandler.js
@@ -1,16 +1,17 @@
-export const globalErrHandler = (err, req, res, next) => { 
-    //stack trace
-    const stack = err?.stack;
-    const message = err?.message;
-    const statusCode = err?.statusCode || 500;
-    res.status(statusCode).json({
-        stack,
-        message,
-    });
+export const globalErrhandler = (err, req, res, next) => {
+  //stack
+  //message
+  const stack = err?.stack;
+  const statusCode = err?.statusCode ? err?.statusCode : 500;
+  const message = err?.message;
+  res.status(statusCode).json({
+    stack,
+    message,
+  });
 };
 
-//404 Handler
+//404 handler
 export const notFound = (req, res, next) => {
-    const err = new Error(`Route ${req.originalUrl} Not Found`);
-    next(err);
-}
+  const err = new Error(`Route ${req.originalUrl} not found`);
+  next(err);
+};
